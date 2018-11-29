c36573d816ee76fd59cfada0eb968951c82e1f2c
makemyA <henrot.paul@gmail.com>
2018-11-29 12:00:32 +0100

    Ajout du board et des soldats pour test

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
 console.log("Test console");
-let pion= document.querySelectorAll('.pion');
 /*objets pour chaque type de pions?
     ex:
         let piece = [
@@ -30,3 +29,17 @@ let pion= document.querySelectorAll('.pion');
   enregistrer les déplacements pour sortir un historique des coups joués + 
   possibilité de relance la partie coup par coup
 */
+//generer l'échiquier
+let boardContainer = document.getElementById('board_container');
+let cell = document.querySelectorAll('.cell');
+for (let i=1; i<65;i++){
+    boardContainer.insertAdjacentHTML('afterbegin',
+    '<li class="cell" id="cell'+i+'"></li>'
+    );   
+};
+for (let i=0; i<8; i++){
+    boardContainer.insertAdjacentHTML('afterend',
+    '<img class="item" src="./images/whitepawn.png">'
+    )
+}
+//générer les pièces
